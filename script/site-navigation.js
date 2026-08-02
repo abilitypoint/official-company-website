6e679b3aacc29e240013c1dcb532a398148666ae
Phinehas Enakireru <phinehasenakireru@gmail.com>
2026-08-02 01:34:14 +0100

    feat: add floating action button with chat and scroll to top

--- script/site-navigation.js
+++ script/site-navigation.js
@@ -1,6 +1,7 @@
 const ui = {
     nav_toggle_button: document.querySelector('.mobile-menu-toggle'),
-    mobile_nav: document.querySelector('.mobile-nav')
+    mobile_nav: document.querySelector('.mobile-nav'),
+    scroll_to_top_button: document.querySelector('#scroll-to-top-button')
 };
 
 
@@ -8,6 +9,11 @@ function registerEventHandlers() {
     ui.mobile_nav.querySelectorAll('a').forEach(a => {
         a.addEventListener('click', () => { ui.mobile_nav.classList.remove('show'); updateNavToggleButtonView(); });
     });
+
+    ui.scroll_to_top_button.addEventListener('click', () => {
+        console.log(34)
+        window.scrollTo({ top: 0, behavior: 'smooth' });
+    })
 }
 
 function updateNavToggleButtonView() {
