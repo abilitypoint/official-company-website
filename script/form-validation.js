fcb93d6356141e835919fa8c40040d6fc8036e81
Phinehas Enakireru <phinehasenakireru@gmail.com>
2026-08-04 08:32:25 +0100

    feat: handle form submission to backend service

--- script/form-validation.js
+++ script/form-validation.js
@@ -1,21 +1,10 @@
-
-
-
-
-
-
-
-
-
-
-
-
-
 const call_back_Form = {
     name_input: document.querySelector('#name'),
     phone_input: document.querySelector('#phone'),
     error_name: document.querySelector('#error-name'),
-    error_phone: document.querySelector('#error-phone')
+    error_phone: document.querySelector('#error-phone'),
+    form_feedback: document.querySelector('#form-feedback'),
+    form: document.querySelector('#call_back_form')
 };
 
 function registerValidationEventHandlers(input_element, error_element) {
@@ -26,7 +15,7 @@ function registerValidationEventHandlers(input_element, error_element) {
         else {
             error_element.hidden = true;
         }
-    })
+    });
 }
 
-export{call_back_Form, registerValidationEventHandlers}
+export { call_back_Form, registerValidationEventHandlers };
--- script/index.js
+++ script/index.js
@@ -1,8 +1,11 @@
 import { ui, handleNavToggleClick, registerEventHandlers } from "./site-navigation.js";
 import { call_back_Form, registerValidationEventHandlers } from "./form-validation.js";
+import { handleSubmit } from "./form-submission.js";
 
 ui.nav_toggle_button.addEventListener('click', (event) => handleNavToggleClick(event));
 registerEventHandlers();
 
 registerValidationEventHandlers(call_back_Form.name_input, call_back_Form.error_name);
 registerValidationEventHandlers(call_back_Form.phone_input, call_back_Form.error_phone)
+
+call_back_Form.form.addEventListener('submit', (event) => handleSubmit(event));
