97b82bf290eff8b3d626bb50f1bd5a0e38395f92
Phinehas Enakireru <phinehasenakireru@gmail.com>
2026-08-04 08:48:45 +0100

    fix: handle fetch exception on form submission

--- script/form-submission.js
+++ script/form-submission.js
@@ -4,23 +4,32 @@ async function handleSubmit(event) {
     const formData = new FormData(call_back_Form.form);
     let name = formData.get("name");
     let phone = formData.get("phone");
-    const response = await fetch('https://abilitypoint-website-0-0-1.onrender.com/form-handler.php', {
-        method: 'POST',
-        headers: { "Content-Type": "application/json" },
-        body: JSON.stringify({ name: name, phone: phone })
-    });
-    
-    console.log(response.status);
-    console.log(await response.text());
 
-    if (response.status > 200) {
+    try {
+        const response = await fetch('https://abilitypoint-website-0-0-1.onrender.com/form-handler.php', {
+            method: 'POST',
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify({ name: name, phone: phone })
+        });
+
+        console.log(response.status);
+        console.log(await response.text());
+
+        if (response.status > 200) {
+            call_back_Form.form_feedback.classList.add('error-message');
+            call_back_Form.form_feedback.textContent = 'Sorry, there has been an error, please try again later';
+        }
+        else {
+            call_back_Form.form_feedback.classList.add('success-message');
+        }
+    } catch (error) {
         call_back_Form.form_feedback.classList.add('error-message');
         call_back_Form.form_feedback.textContent = 'Sorry, there has been an error, please try again later';
+        console.log(error)
     }
-    else {
-        call_back_Form.form_feedback.classList.add('success-message');
-    }
+
     call_back_Form.form_feedback.hidden = false;
+
 }
 
 export{handleSubmit}
